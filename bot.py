772ae3d608861cacee876172fcd477470750b6eb
Mateus Santos <65752974+M-de-Mateus@users.noreply.github.com>
2022-08-24 15:26:50 -0300

    Ajustando importações e ocultando token

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
-import telebot as tb
+import os
 import sqlite3
+import telebot as tb
 
-
-bd = r'C:\Users\Mateu\Desktop\pastas\github\Reconhecimento-facial-pca\M-de-Mateus\Reconhecimento-facial-pca\bd\pca.db'
-
-chave_api = 'TOKEN'
+with open(os.path.relpath(r'C:\Users\Mateu\Desktop\token.txt'), 'r', encoding='utf-8') as token:
+    chave = token.read()
+chave_api = chave
 bot = tb.TeleBot(chave_api)
+
+bd = os.path.relpath(r'bd\pca.db')
 
 
 @bot.message_handler(commands=['start'])
@@ -43,6 +45,37 @@ def ativa_cadastro(mensagem):
                     conn.close()
     except ValueError:
         bot.send_message(mensagem.chat.id, 'Digite o CPF após o comando! (Ex.: /ativar 11122233345)')
+
+
+@bot.message_handler(commands=['desativar'])
+def desativa_cadastro(mensagem):
+    conn = sqlite3.connect(bd, check_same_thread=False)
+    cursor = conn.cursor()
+    try:
+        msg = mensagem.text
+        cmd, cpf = msg.split(" ", 1)
+        if len(cpf) != 11:
+            bot.send_message(mensagem.chat.id, 'Não consegui encontrar o seu CPF, verifique se o digitou corretamente!')
+        else:
+            cursor.execute(f'''SELECT cpfResponsavel FROM responsavel WHERE cpfResponsavel = "{cpf}"''')
+            for linha in cursor.fetchall():
+                if cpf not in str(linha[0]):
+                    bot.send_message(mensagem.chat.id,
+                                     'Não consegui encontrar o seu CPF, entre em contato com a escola e '
+                                     'verifique seu cadastro!')
+                    conn.close()
+                else:
+                    cursor.execute(
+                        f'''UPDATE responsavel SET idChatResponsavel = "{0}" WHERE 
+                        cpfResponsavel = "{cpf}"''')
+                    conn.commit()
+                    bot.send_message(mensagem.chat.id, 'Cadastro desativado com sucesso, agora você não receberá '
+                                                       'notificações '
+                                                       'quando seu filho entrar ou sair da escola!')
+                    bot.send_message(mensagem.chat.id, 'Para mais comandos digite /comandos')
+                    conn.close()
+    except ValueError:
+        bot.send_message(mensagem.chat.id, 'Digite o CPF após o comando! (Ex.: /desativar 11122233345)')
 
 
 bot.polling()
